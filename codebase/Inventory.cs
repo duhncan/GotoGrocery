8535a1016cb5839efa81c371d7f8511d3cea5168
DanielErle <102575825+DanielErle@users.noreply.github.com>
2022-04-23 18:57:53 +1000

    AddItem

--- codebase/Inventory.cs
+++ codebase/Inventory.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,27 +6,53 @@ using System.Threading.Tasks;
 
 namespace GotoGrocery
 {
-    class Inventory
-    {
-        //pull stuff from databse and chuck it all in a big ol' array then 
-        //read that bad boi out and dump it all in a csv file
-        List<List<string>> myList = new List<List<string>>();
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
 
-        //foreach row in databse, make new sublist with data from each
-        //column and add to big list
-        //foreach (row in databse)
-        //{
-        //    myList.Add(new List<string> { "id", "fname", "lname" etc.(item stuff not member stuff) });
-        //}
-        // inventory class should take data from gui, check it for integrity/verification purposes, then send to the databse class to be uploaded to the database
-        //// To iterate over it.
-        //foreach (List<string> subList in myList)
-        //{
-        //    foreach (string item in subList)
-        //    {
-        //        //put csv export stuff in here
-        //        Console.WriteLine(item);
-        //    }
-        //}
+    namespace GoToGrocery
+    {
+        class Inventory
+        {
+            private DatabaseConnection db;
+
+            public Inventory()
+            {
+                
+            }
+
+            //change this to return an int, value of int is determined by which input failed the check
+            public bool validateProduct(string _productname, int _inventorylevel, string  _productsize, int _shelfquantity, int _orderamount)
+            {
+                DatabaseConnection db = new DatabaseConnection();
+
+                //check strings not empty and counts not less than or equal to zero
+                if (_productname == "" || _inventorylevel <= 0 || _productsize == "" || _shelfquantity <= 0 || _orderamount <= 0)
+                {
+                    db.AddProduct(_productname, _inventorylevel, _productsize, _shelfquantity, _orderamount);
+                    return true;
+                }
+                return false;
+            }
+            
+
+            // inventory class should take data from gui, check it for integrity/verification purposes, then send to the databse class to be uploaded to the database
+            //// To iterate over it.
+            //foreach (List<string> subList in myList)
+            //{
+            //    foreach (string item in subList)
+            //    {
+            //        //put csv export stuff in here
+            //        Console.WriteLine(item);
+            //    }
+            //}
+
+
+            //after input checking, add to database with this:
+
+        }
     }
+
 }
