55964f2cd9a5d94bf72f53d46224c4a408708fe6
DanielErle <102114479@student.swin.edu.au>
2022-05-14 14:28:19 +1000

    completed inventory testing

--- codebase/Testing/InventoryTesting.cs
+++ codebase/Testing/InventoryTesting.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using GotoGrocery;
+using GoToGrocery;
 
 namespace Testing
 {
@@ -26,12 +26,54 @@ namespace Testing
             Assert.IsNotNull(_testInventory); // Ensure object is created and existing
         }
 
-        public void TestValidation()
+        //Inventory order: Product name, inventory level, product size, shelf quantity, order amount
+        [Test]
+        public void TestValidationAllValid()
         {
-            Assert.Pass();
+            int errorcode = _testInventory.validateProduct("name", "12", "14 kg", "22", "16");
+            Assert.IsTrue(errorcode == 0);
         }
 
+        [Test]
+        public void TestValidationNameInvalid()
+        {
+            int errorcode = _testInventory.validateProduct("", "12", "14 kg", "22", "16");
+            Assert.IsTrue(errorcode == 1);
+        }
 
+        [Test]
+        public void TestValidationLevelInvalid()
+        {
+            int errorcode = _testInventory.validateProduct("name", "inventorylevel", "14 kg", "22", "16");
+            Assert.IsTrue(errorcode == 2);
+        }
 
+        [Test]
+        public void TestValidationSizeInvalid()
+        {
+            int errorcode = _testInventory.validateProduct("name", "12", "productsize", "22", "16");
+            Assert.IsTrue(errorcode == 3);
+        }
+
+        [Test]
+        public void TestValidationShelfInvalid()
+        {
+            int errorcode = _testInventory.validateProduct("name", "12", "14 kg", "shelfquantity", "16");
+            Assert.IsTrue(errorcode == 4);
+        }
+
+        [Test]
+        public void TestValidationOrderInvalid()
+        {
+            int errorcode = _testInventory.validateProduct("name", "12", "14 kg", "22", "orderamount");
+            Assert.IsTrue(errorcode == 5);
+        }
+
+        [Test]
+        public void TestValidationMultipleInvalid()
+        {
+            int errorcode = _testInventory.validateProduct("", "inventorylevel", "productsize", "shelfquantity", "orderamount");
+            Assert.IsTrue(errorcode == 1);
+        }
     }
 }
